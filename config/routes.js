c2c45f75127833af97cbaf2c40afd28db1df358c
Pascale Pierre <pascaleee@gmail.com>
2019-01-26 12:42:13 -0600

    got register working

--- config/routes.js
+++ config/routes.js
@@ -1,32 +1,71 @@
-const axios = require('axios');
-
-const { authenticate } = require('../auth/authenticate');
+const axios = require("axios");
+const db = require('../database/dbConfig');
+const { authenticate } = require("../auth/authenticate");
+const bcrypt = require('bcryptjs');
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
+  server.post("/api/register", register);
+  server.post("/api/login", login);
+  server.get("/api/jokes", authenticate, getJokes);
 };
 
+const secret = "secretsecretsecret";
+
+function generateToken(user) {
+  const payload = {
+    username: user.username
+  };
+  const options = {
+    expiresIn: "1hr",
+    jwtid: "12345"
+  };
+  return jwt.sign(payload, secret, options);
+}
+
+
 function register(req, res) {
-  // implement user registration
+  const user = req.body;
+  user.password = bcrypt.hashSync(user.password, 12)
+  db.insert(user)
+    .then(response => {
+      res.status(201).json({ message: 
+        "yay, you get an account!"})
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: "sorry, no account for you"
+      })
+    })
 }
 
 function login(req, res) {
-  // implement user login
+  const login = req.body;
+  if (login.username && login.password) {
+     db.findByUsername(login.username)
+        .then(users => {
+           if(users.length && bcrypt.compareSync(login.password, users[0].password)) {
+           const token = generateToken(users[0])
+           res.status(200).json({token});
+            } else { res.status(404).send("You shall not pass!");}
+        })
+        .catch(err => {
+           res.status(500).send(err);
+        });
+  } else  res.status(400).json({err: "please provide a username and password"});
+
 }
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
+    headers: { accept: "application/json" }
   };
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
       res.status(200).json(response.data.results);
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err });
     });
 }
--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,4 +2,26 @@ const knex = require('knex');
 
 const knexConfig = require('../knexfile.js');
 
-module.exports = knex(knexConfig.development);
+const db = knex(knexConfig.development);
+
+function insert(user){
+  return db('users').insert(user)
+}
+
+function findUsers(){
+  return db('users').select('id', 'username');
+}
+
+function findByUsername(username){
+  return db('users').where('username', username);
+}
+
+function findByUserId(id){
+  return db('users').where({id}).first()
+}
+
+
+module.exports = {
+   insert, findByUsername, findUsers, findByUserId
+     
+}
